ad7d7d15ff4a53d4677e852f4e00f90e28213d4d
ngan <vutran@sheridancollege.ca>
2024-06-05 14:39:21 -0400

    Lesson 5

--- lesson-note.js
+++ lesson-note.js
@@ -1,82 +1,18 @@
-const http = require('http');
-const fs = require('fs');
+/* 
 
-const server = http.createServer((req, res) => {
-  // console.log(req.url);
+  Nodemon helps to restart the server when the file changes: npm i -g nodemon --> install globally
 
-  /* 
+  Run server file in terminal: nodemon lesson-note 
 
-    set header content type: text/html, text/plain
-
-  */
-  // res.setHeader('Content-Type', 'text/html');
-
-  // res.write('<p>hello, ninjas</p>');
-  // res.write('<p>hello again, ninjas</p>');
-  
-  // res.end();
-
-  
-  
-  
-  /* 
-
-    send html file
-
-  */
-  // const readStream = fs.createReadStream("./views/index.html", { encoding: "utf8" })
-  // readStream.on("data", chunk => {
-  //   res.write(chunk) --> if you only write one thing, you can pass it into the end() function
-  //   res.end(chunk)
-  // })
-
-
-
-  /* 
-
-    Status Code
-
-    + syntax: res.statusCode
-
-    + youtube tutorial: https://www.youtube.com/watch?v=DQD00NAUPNk&list=PL4cUxeGkcC9jsz4LDYc6kv3ymONOKxwBU&index=4&t=779s
-
-    Redirect: 
-
-    + syntax: res.setHeader(name, value)
-
-    + example: res.setHeader("Location", "/about") --> The Location header indicates the URL to which the client should be redirected
-
-  */
-  let path = "./views/";
-  switch (req.url) {
-    case "/":
-      path += "index.html";
-      res.statusCode = 200;
-      break;
-    case "/about":
-      path += "about.html";
-      res.statusCode = 200;
-      break;
-    case "/about-us":
-      res.statusCode = 301;
-      res.setHeader("Location", "/about");
-      res.end();
-      break;
-    default:
-      path += "404.html";
-      res.statusCode = 404;
-  }
-
-});
+*/
 
 
 
 
 /* 
 
-  localhost is the default value for 2nd argument
+  npm i --save lodash
 
 */
-server.listen(3000, 'localhost', () => {
-  console.log('listening for requests on port 3000');
-});
+const _ = require("lodash")
+console.log(_.random(0, 20))
